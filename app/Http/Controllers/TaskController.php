ff82507abfc69a93a799ea32eff7878c52455137
渡辺宣人 <watanabeno44@gmail.com>
2020-03-29 11:45:27 +0900

    0329

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Folder;
 use App\Task;
 use Illuminate\Http\Request;
+use App\Http\Requests\CreateTask;
 
 class TaskController extends Controller
 {
@@ -17,12 +18,60 @@ class TaskController extends Controller
     $current_folder = Folder::find($id);
 
     // 選ばれたフォルダに紐づくタスクを取得する
-    $tasks = Task::where('folder_id', $current_folder->id)->get();
+    $tasks = $current_folder->tasks()->get(); // ★
 
     return view('tasks/index', [
         'folders' => $folders,
         'current_folder_id' => $current_folder->id,
         'tasks' => $tasks,
+    ]);
+}
+
+    public function showCreateForm(int $id)
+    {
+        return view('tasks/create', [
+            'folder_id' => $id
+        ]);
+    }
+
+    public function create(int $id, CreateTask $request)
+    {
+        $current_folder = Folder::find($id);
+
+        $task = new Task();
+        $task->title = $request->title;
+        $task->due_date = $request->due_date;
+
+        $current_folder->tasks()->save($task);
+
+        return redirect()->route('tasks.index', [
+            'id' => $current_folder->id,
+        ]);
+    }
+
+    public function showEditForm(int $id, int $task_id)
+{
+    $task = Task::find($task_id);
+
+    return view('tasks/edit', [
+        'task' => $task,
+    ]);
+}
+
+public function edit(int $id, int $task_id, EditTask $request)
+{
+    // 1
+    $task = Task::find($task_id);
+
+    // 2
+    $task->title = $request->title;
+    $task->status = $request->status;
+    $task->due_date = $request->due_date;
+    $task->save();
+
+    // 3
+    return redirect()->route('tasks.index', [
+        'id' => $task->folder_id,
     ]);
 }
 }
--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,9 @@ use App\Http\Middleware\HelloMiddleware;
 // Route::post('person/del', 'PersonController@remove');
 
 Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
+Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
+Route::post('/folders/create', 'FolderController@create');
+Route::get('/folders/{id}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
+Route::post('/folders/{id}/tasks/create', 'TaskController@create');
+Route::get('/folders/{id}/tasks/{task_id}/edit', 'TaskController@showEditForm')->name('tasks.edit');
+Route::post('/folders/{id}/tasks/{task_id}/edit', 'TaskController@edit');
